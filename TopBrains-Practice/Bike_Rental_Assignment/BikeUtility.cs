f20a9d645a25e946cc0e4d493a195e7d237341ab
Aryan Narayan <aryanshrivastava788@gmail.com>
2026-08-05 17:54:28 +0530

    Added Bike Rental Assignment

---
+++ TopBrains-Practice/Bike_Rental_Assignment/BikeUtility.cs
@@ -0,0 +1,32 @@
+public class BikeUtility
+{
+    public void AddBikeDetails(string model, string brand, int pricePerDay)
+    {
+        Bike bike = new Bike();
+        bike.Model = model;
+        bike.Brand = brand;
+        bike.PricePerDay = pricePerDay;
+
+        int key = Program.bikeDetails.Count + 1;
+        Program.bikeDetails.Add(key, bike);
+    }
+
+    public SortedDictionary<string, List<Bike>> GroupBikesByBrand()
+    {
+        SortedDictionary<string, List<Bike>> groupedBikes = new SortedDictionary<string, List<Bike>>();
+
+        foreach (KeyValuePair<int, Bike> item in Program.bikeDetails)
+        {
+            Bike bike = item.Value;
+
+            if (!groupedBikes.ContainsKey(bike.Brand))
+            {
+                groupedBikes[bike.Brand] = new List<Bike>();
+            }
+
+            groupedBikes[bike.Brand].Add(bike);
+        }
+
+        return groupedBikes;
+    }
+}
